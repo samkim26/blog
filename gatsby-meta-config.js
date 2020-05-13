d6a629d83b53aaec381632ca2ec830674744708f
Sam Myunghwan Kim <samkim2626@gmail.com>
2020-05-13 21:33:49 +0900

    Initial commit

---
+++ gatsby-meta-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: 'Myunghwan',
+  description: '명환이의 블로그 - 복쭈 No.1',
+  author: 'Myung-hwan',
+  introduction: 'Nothing changes if nothing changes',
+  siteUrl: 'https://gatsby-starter-bee.netlify.com', // Your blog site url
+  social: {
+    twitter: '', // Your Twitter account
+    github: '', // Your GitHub account
+    medium: '', // Your Medium account
+    facebook: '', // Your Facebook account
+    linkedin: '', // Your LinkedIn account
+  },
+  icon: '', // Add your favicon
+  keywords: ['blog'],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: '', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
