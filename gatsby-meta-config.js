acc50e7937d4d70002af56ec28773f27762ea247
Myunghwan Kim <samkim2626@gmail.com>
2020-05-14 13:06:43 +0900

    fix: change image format

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -11,7 +11,7 @@ module.exports = {
     facebook: '', // Your Facebook account
     linkedin: '', // Your LinkedIn account
   },
-  icon: 'content/assets/favicon.ico', // Add your favicon
+  icon: 'content/assets/favicon.png', // Add your favicon
   keywords: ['blog'],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
