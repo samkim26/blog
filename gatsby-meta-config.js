3dde25ab6db62d971b43d02b6e61154f9df5e493
Myunghwan Kim <samkim2626@gmail.com>
2021-06-24 12:16:18 +0900

    Update github account

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -6,7 +6,7 @@ module.exports = {
   siteUrl: 'https://hwanny.netlify.app/', // Your blog site url
   social: {
     twitter: '', // Your Twitter account
-    github: 'samkim26', // Your GitHub account
+    github: 'samkimuel', // Your GitHub account
     medium: '', // Your Medium account
     facebook: '', // Your Facebook account
     linkedin: '', // Your LinkedIn account
@@ -15,7 +15,7 @@ module.exports = {
   keywords: ['blog'],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'samkim26/hwanny.netlify.app', // Your repository for archive comment
+    utterances: 'samkimuel/hwanny.netlify.app', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
